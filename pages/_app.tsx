9f82c7d375a694fa958b87bbe4b2edcd0b649142
Jay Harris <jay.harris@outlook.co.nz>
2021-12-01 21:03:20 +0100

    PoC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/global.css'
+import '../styles/modest.css';
+import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
